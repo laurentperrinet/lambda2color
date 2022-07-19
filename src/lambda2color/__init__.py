bfdb4237040e0880e68645c3198757790092df3b
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2022-07-19 12:57:25 +0200

    tagging for PyPI

--- src/lambda2color/__init__.py
+++ src/lambda2color/__init__.py
@@ -6,4 +6,4 @@ This is a simple library to transform a given light wavelength into the correspo
 # The version of this package. There's no comprehensive, official list of other
 # magic constants, so we stick with this one only for now. See also this conversation:
 # https://stackoverflow.com/questions/38344848/is-there-a-comprehensive-table-of-pythons-magic-constants
-__version__ = "0.1"
+__version__ = "0.2"
