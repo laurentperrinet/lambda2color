bafbd367d0b68d09753d2ab5cc3eb1cc6b6eb8af
Laurent Perrinet <laurent.perrinet@univ-amu.fr>
2022-07-19 12:49:52 +0200

    going to a simpler version

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+from setuptools import setup, find_packages
+
+NAME = "LogGabor"
+import LogGabor
+VERSION = LogGabor.__version__ # << to change in __init__.py
+
+setup(
+    name = NAME,
+    version = VERSION,
+    packages = find_packages(exclude=['contrib', 'docs', 'tests']),
+     author = "Laurent Perrinet INT - CNRS",
+    author_email = "Laurent.Perrinet@univ-amu.fr",
+    description = "A log-Gabor pyramid is an oriented multiresolution scheme for images inspired by biology.",
+    long_description=open("README.md", 'r', encoding='utf-8').read(),
+    license = "GPLv2",
+    install_requires=['SLIP'],
+    extras_require={
+                'html' : [
+                         'vispy',
+                         'matplotlib'
+                         'jupyter>=1.0']
+    },
+    keywords = ('computational neuroscience', 'simulation', 'analysis', 'visualization', 'biologically-inspired', 'computer vision'),
+    url = 'https://github.com/bicv/' + NAME, # use the URL to the github repo
+    download_url = 'https://github.com/bicv/' + NAME + '/tarball/' + VERSION,
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: POSIX',
+                   'Topic :: Scientific/Engineering',
+                   'Topic :: Utilities',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.5',
+                  ],
+     )
--- tests/test_something.py
+++
@@ -1,7 +0,0 @@
-"""Test the lambda2color module."""
-
-# from lambda2color import Lambda2color
-#
-# def test_something() -> None:
-#     """TODO: test something useful here."""
-#     assert True
